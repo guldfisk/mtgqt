bbc07c8a7670068d4b4090ddec8f729e900dd45f
guldfisk <ce.guldfisk@gmail.com>
2019-09-26 16:22:10 +0200

    urself

--- mtgqt/pixmapload/pixmaploader.py
+++ mtgqt/pixmapload/pixmaploader.py
@@ -7,118 +7,103 @@ from promise import Promise
 from PyQt5.QtGui import QPixmap
 
 from mtgimg.load import Loader as ImageLoader, TaskAwaiter
-from mtgimg.interface import ImageRequest, picturable
-
-
-# class SingleAccessDict(dict):
-#
-# 	def __init__(self):
-# 		super().__init__()
-# 		self._lock = Lock()
-#
-# 	def __getitem__(self, k):
-# 		with self._lock:
-# 			return super().__getitem__(k)
-#
-# 	def __setitem__(self, k, v) -> None:
-# 		with self._lock:
-# 			super().__setitem__(k, v)
+from mtgimg.interface import ImageRequest, pictureable
 
 
 class _PixmapConverter(object):
 
-	def __init__(self):
-		self._pixmaps = dict()  # type: t.Dict[ImageRequest, QPixmap]
-		self._processing = TaskAwaiter()
+    def __init__(self):
+        self._pixmaps = dict()  # type: t.Dict[ImageRequest, QPixmap]
+        self._processing = TaskAwaiter()
 
-	def get_pixmap(self, image_request: t.Optional[ImageRequest], image: Image.Image):
-		try:
-			return self._pixmaps[image_request]
-		except KeyError:
-			pass
+    def get_pixmap(self, image_request: t.Optional[ImageRequest], image: Image.Image):
+        try:
+            return self._pixmaps[image_request]
+        except KeyError:
+            pass
 
-		event, in_progress = self._processing.get_condition(image_request)
+        event, in_progress = self._processing.get_condition(image_request)
 
-		if in_progress:
-			event.wait()
+        if in_progress:
+            event.wait()
 
-			return self._pixmaps[image_request]
+            return self._pixmaps[image_request]
 
-		if image_request and os.path.exists(image_request.path):
-			pixmap = QPixmap(image_request.path)
+        if image_request and os.path.exists(image_request.path):
+            pixmap = QPixmap(image_request.path)
 
-		else:
-			# noinspection PyCallByClass
-			self._pixmaps[image_request] = pixmap = QPixmap.fromImage(
-				ImageQt.ImageQt(
-					image
-				)
-			)
+        else:
+            self._pixmaps[image_request] = pixmap = QPixmap.fromImage(
+                ImageQt.ImageQt(
+                    image
+                )
+            )
 
-		self._pixmaps[image_request] = pixmap
+        self._pixmaps[image_request] = pixmap
 
-		event.set_value(None)
+        event.set_value(None)
 
-		return pixmap
+        return pixmap
 
 
 class PixmapLoader(object):
 
-	def __init__(
-		self,
-		pixmap_executor: t.Union[Executor, int] = None,
-		*,
-		imageable_executor: t.Union[Executor, int] = None,
-		printing_executor: t.Union[Executor, int] = None,
-		image_loader: ImageLoader = None,
-	):
-		self._image_loader = (
-			ImageLoader(
-				imageable_executor = imageable_executor,
-				printing_executor = printing_executor,
-			) if image_loader is None else
-			image_loader
-		)
+    def __init__(
+        self,
+        pixmap_executor: t.Union[Executor, int] = None,
+        *,
+        imageable_executor: t.Union[Executor, int] = None,
+        printing_executor: t.Union[Executor, int] = None,
+        image_loader: ImageLoader = None,
+    ):
+        self._image_loader = (
+            ImageLoader(
+                imageable_executor = imageable_executor,
+                printing_executor = printing_executor,
+            ) if image_loader is None else
+            image_loader
+        )
 
-		self._pixmap_executor = (
-			pixmap_executor
-			if pixmap_executor is isinstance(pixmap_executor, Executor) else
-			ThreadPoolExecutor(
-				max_workers = pixmap_executor if isinstance(pixmap_executor, int) else 10
-			)
-		)
+        self._pixmap_executor = (
+            pixmap_executor
+            if pixmap_executor is isinstance(pixmap_executor, Executor) else
+            ThreadPoolExecutor(
+                max_workers = pixmap_executor if isinstance(pixmap_executor, int) else 10
+            )
+        )
 
-		self._pixmap_converter = _PixmapConverter()
+        self._pixmap_converter = _PixmapConverter()
 
-		self._default_image = self._image_loader.get_default_image().get()
+        self._default_image = self._image_loader.get_default_image()
 
-	def get_pixmap(
-		self,
-		pictured: picturable = None,
-		back: bool = False,
-		crop: bool = False,
-		image_request: ImageRequest = None
-	) -> Promise:
+    def get_pixmap(
+        self,
+        pictured: pictureable = None,
+        back: bool = False,
+        crop: bool = False,
+        image_request: ImageRequest = None
+    ) -> Promise:
 
-		_image_request = (
-			ImageRequest(pictured, back, crop)
-			if image_request is None else
-			image_request
-		)
+        _image_request = (
+            ImageRequest(pictured, back = back, crop = crop)
+            if image_request is None else
+            image_request
+        )
 
-		return self._image_loader.get_image(
-			image_request = _image_request,
-		).then(
-			lambda image:
-				self._pixmap_executor.submit(
-					self._pixmap_converter.get_pixmap,
-					_image_request,
-					image,
-				)
-		)
+        return self._image_loader.get_image(
+            image_request = _image_request,
+        ).then(
+            lambda image:
+            self._pixmap_executor.submit(
+                self._pixmap_converter.get_pixmap,
+                _image_request,
+                image,
+            )
+        )
 
-	def get_default_pixmap(self) -> QPixmap:
-		return self._pixmap_converter.get_pixmap(None, self._default_image)
-		# return self._image_loader.get_default_image().then(
-		# 	lambda image: self._pixmap_converter.get_pixmap(None, image)
-		# )
+    def get_default_pixmap(self) -> QPixmap:
+        return self._pixmap_converter.get_pixmap(None, self._default_image)
+
+    # return self._image_loader.get_default_image().then(
+    # 	lambda image: self._pixmap_converter.get_pixmap(None, image)
+    # )
--- setup.py
+++ setup.py
@@ -4,32 +4,33 @@ from setuptools import setup
 
 
 def package_files(directory):
-	paths = []
-	for path, directories, file_names in os.walk(directory):
-		for filename in file_names:
-			paths.append(os.path.join('..', path, filename))
-	return paths
+    paths = []
+    for path, directories, file_names in os.walk(directory):
+        for filename in file_names:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
 
 extra_files = package_files('mtgqt')
 
 setup(
-	name = 'mtgqt',
-	version = '1.0',
-	packages = ['mtgqt'],
-	package_data={'': extra_files},
-	dependency_links = [
-		'https://github.com/guldfisk/mtgorp/tarball/master#egg=mtgorp-1.0',
-		'https://github.com/guldfisk/orp/tarball/master#egg=orp-1.0',
-		'https://github.com/guldfisk/mtgimg/tarball/master#egg=mtgimg-1.0',
-	],
-	include_package_data = True,
-	install_requires = [
-		'appdirs',
-		'mtgorp',
-		'mtgimg',
-		'orp',
-		'pillow',
-		'promise',
-		'PyQt5',
-	],
+    name = 'mtgqt',
+    version = '1.0',
+    packages = ['mtgqt'],
+    package_data={'': extra_files},
+    dependency_links = [
+        'https://github.com/guldfisk/mtgorp/tarball/master#egg=mtgorp-1.0',
+        'https://github.com/guldfisk/orp/tarball/master#egg=orp-1.0',
+        'https://github.com/guldfisk/mtgimg/tarball/master#egg=mtgimg-1.0',
+    ],
+    include_package_data = True,
+    install_requires = [
+        'appdirs',
+        'mtgorp',
+        'mtgimg',
+        'orp',
+        'pillow',
+        'promise',
+        'PyQt5',
+    ],
 )
